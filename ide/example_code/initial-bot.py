64e7f7a4f7303bf5845fd2e7847e7f445a0edeb0
Daniel Steuer <hk-github@sockenbuegeln.de>
2022-05-27 22:01:54 +0200

    example code: add initial bot for python language

---
+++ ide/example_code/initial-bot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import math
+
+
+# This is your bot's startup function. Here you can set your snake's colors,
+# set up persistent variables, etc.
+def init(api):
+    # remove the default color
+    api.clearColors()
+
+    # I'm green!
+    api.addColor(40, 255, 0)
+    api.addColor(20, 128, 0)
+    api.addColor(10,  64, 0)
+    api.addColor(20, 128, 0)
+    return True
+
+
+# This function will be called by the framework on every step. Here you decide
+# where to move next!
+#
+# Use the provided Api object to interact with the world and make sure you set
+# the following values as the return value:
+#
+# success:    True in case you want to keep running
+# deltaAngle: Set your relative movement angle
+# boost:      Set this to true to move faster, but you will loose mass.
+#
+# The Api object also provides information about the world around you. See the
+# documentation for more details.
+def step(api):
+    deltaAngle = 0.0
+    boost      = False
+    success    = True
+
+    # let's start by moving in a large circle. Please note that all angles are
+    # represented in radians, where -π to +π is a full circle.
+    deltaAngle = 0.001
+
+    # check for other snakes
+    for seg in api.getSegments():
+        if not seg.isSelf and seg.dist < 20:
+            # you can send log messages to your browser or any other viewer with the
+            # appropriate Viewer Key.
+            api.log("Oh no, I'm going to die!")
+            break
+
+    # finding food is quite similar
+
+    # Signal that everything is ok. Return false here if anything goes wrong but
+    # you want to shut down cleanly
+
+    return success, deltaAngle, boost
